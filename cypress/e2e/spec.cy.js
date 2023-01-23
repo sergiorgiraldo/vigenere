2aa7279759228684b5ee36b17d0281d6ec04315e
Sergio Giraldo <sergio@giraldo.com.br>
2023-01-23 16:44:59 +0100

    cypress testing:: by sergio giraldo @ 20230123T1644CET, gpg signed

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,36 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('must have fields for key and texts', () => {
+    cy.get("#secret-key");
+    cy.get("#original");
+    cy.get("#result");
+  })
+
+  it('must have buttons for encrypt and decrypt', () => {
+    cy.get("#btnEncrypt");
+    cy.get("#btnDecrypt");
+  })
+
+  it('btn encrypt', () => {
+    cy.get("#secret-key").type("TEST");
+    cy.get("#original").type("foo");
+
+    cy.get("#btnEncrypt").then($btn => {
+        $btn.click();
+        cy.get("#result").should('have.value', 'ysg');
+      });
+  })
+
+  it('btn decrypt', () => {
+    cy.get("#secret-key").type("TEST");
+    cy.get("#original").type("ysg");
+
+    cy.get("#btnDecrypt").then($btn => {
+        $btn.click();
+        cy.get("#result").should('have.value', 'foo');
+      });
   })
 })
